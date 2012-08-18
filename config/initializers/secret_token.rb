c5c910bcf07a2aff61ccce6b7eccd3a3d5d2aba6
B.J. Allen <bjallen@mac.com>
2012-08-18 09:04:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tally::Application.config.secret_token = 'cfd03f38ea0736b9e7777aac118dd1c0ac08d9eba22c01b93f69c5c3f864133cdc4a278a3cc02c050dd600a12474dd7f007be125df55213fe5f739260081fb82'
